709bc08a29b667703a949e2c7692b726392a1c7f
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 16:02:01 +0200

    Commiting week10_0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -12,25 +12,16 @@ Retrieve the librarian for a library.
 """
 # Define the query for each of the relationship
 # Query all books by a specific author.
+author = Author.objects.get(name="John Doe")
+books = author.books.all()
+print(books)
+
 # List all books in a library.
+library = Library.objects.get(name="library_name")
+books = library.books.all()
+print(books)
+
 # Retrieve the librarian for a library.
-def query_samples():
+librarian = library.librarian.all()
+print(librarian)
 
-          # List all books in a library.
-          library = Library.objects.get(name="library_name")
-          books = library.books.all()
-          print(books)
-
-          # Query all books by a specific author.
-          author = Author.objects.get(name="John Doe")
-          books = author.books.all()
-          print(books)
-
-          # Retrieve the librarian for a library.
-          librarian = library.librarian.all()
-          print(librarian)
-
-# Call the function to execute the queries
-query_samples()
-# Output:
-# <QuerySet [<Book: Book object (1)>, <Book: Book object (2)>]>
