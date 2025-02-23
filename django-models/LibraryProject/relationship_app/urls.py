ffd17a7f2bda1caab48736d058bb431bcd7aca09
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 17:15:35 +0200

    Commiting 1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,10 +4,10 @@ Configure URL Patterns:
 Edit relationship_app/urls.py to include URL patterns that route to the newly created views. Make sure to link both the function-based and class-based views.
 """
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-          path('books/', views.book_list, name='book_list'),
-          path('library/<int:pk>/', views.LibraryBookListView.as_view(), name='library_book_list'),
+          path('books/',list_books, name='book_list'),
+          path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_book_list'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,30 +4,11 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
 
-"""
-Objective: Develop proficiency in creating both function-based and class-based views in Django, and configuring URL patterns to handle web requests effectively. This task will help you understand different ways to define views and manage URL routing in Django.
-
-Task Description:
-In your existing Django project, enhance the relationship_app by adding new views that display information about books and libraries. Implement both function-based and class-based views to handle these displays and configure the URL patterns to route these views correctly.
-
-Steps:
-Implement Function-based View:
-
-Create a function-based view in relationship_app/views.py that lists all books stored in the database.
-This view should render a simple text list of book titles and their authors."""
-
 # LibraryProject/relationship_app/views.py doesn't contain: ["relationship_app/list_books.html"]
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/book_list.html', {'books': books})
 
-"""
-Implement Class-based View:
-
-Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
-Utilize Django’s ListView or DetailView to structure this class-based view.
-"""
-#LibraryProject/relationship_app/views.py doesn't contain: ["relationship_app/library_detail.html", "from .models import Library"]
 class LibraryBookListView(ListView):
           model = Book
           template_name = 'relationship_app/library_detail.html'
