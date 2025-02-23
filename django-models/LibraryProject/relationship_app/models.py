fcc52ee442399acbd6eb2e16c8ebf5bc346cc923
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 15:48:59 +0200

    Commiting week10_0

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -17,18 +17,29 @@ library: OneToOneField to Library."""
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    # Add __str__ method to Author model
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Library(models.Model):
     name = models.CharField(max_length=255)
     books = models.ManyToManyField(Book, related_name="libraries")
 
+    def __str__(self):
+        return self.name
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
     library = models.OneToOneField(Library, related_name="librarian", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
 Librarian = Librarian.objects.prefetch_related("library")
 Book = Book.objects.prefetch_related("author")
