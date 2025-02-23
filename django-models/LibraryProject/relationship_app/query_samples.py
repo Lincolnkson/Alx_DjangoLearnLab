b3fc941fd7f24c25f7025f6cdd60276288150fd8
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 16:37:53 +0200

    Commiting week10_0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import Author, Book, Library,Librarian
 # Define the query for each of the relationship
 # Query all books by a specific author.
 #["Author.objects.get(name=author_name)", "objects.filter(author=author)"]
@@ -15,7 +15,12 @@ books = Library.objects.get(name=library_name)
 print(books.books.all())
 
 # # Retrieve the librarian for a library.
-librarian = Library.objects.get(name="library_name").librarian.all()
+# librarian = Library.objects.get(name="library_name").librarian.all()
+# print(librarian)
+
+#LibraryProject/relationship_app/query_samples.py doesn't contain: ["Librarian.objects.get(library="]
+
+librarian = Librarian.objects.get(library=library_name)
 print(librarian)
 
 # print(Library.objects.get(name="library_name").books.all())
