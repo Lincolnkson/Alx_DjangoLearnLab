a681092e8ece9be68c86690dcc371619b198c8a2
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 20:48:32 +0200

    02

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import list_books,LibraryDetailView, login_view, logout_view
+from .views import list_books,LibraryDetailView, login_view, logout_view, register
  #["from .views import list_books", "LibraryDetailView"]
-
+"""
+["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
+"""
 urlpatterns = [
           path('books/',list_books, name='list_books'),
           path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_book_list'),
           path('login/', login_view),
           path('logout/', logout_view, name='logout'),
+          path('register/', register, name='register'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -66,3 +66,13 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'You have been successfully logged out.')
     return render(request, 'relationship_app/logout.html')
+
+# create a view for the registration form
+class register(FormView):
+        template_name = 'relationship_app/register.html'
+        form_class = UserCreationForm
+        success_url = reverse_lazy('login')
+     
+        def form_valid(self, form):
+            form.save()
+            return super().form_valid(form)
