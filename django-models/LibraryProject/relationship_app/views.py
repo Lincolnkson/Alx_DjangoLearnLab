c9f6e8ae302ff6f332a5018db9abd7cd0b1eda49
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 17:18:18 +0200

    Commiting 1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from .models import Book,Library
+from .models import Book
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-
+from .models import Library
 
 # LibraryProject/relationship_app/views.py doesn't contain: ["relationship_app/list_books.html"]
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/book_list.html', {'books': books})
+
 
 class LibraryBookListView(ListView):
           model = Book
