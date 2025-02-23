8697c5045a7fb2192fa8bc7e859378313094d783
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-02-23 15:57:32 +0200

    Commiting week10_0

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -16,18 +16,15 @@ Retrieve the librarian for a library.
 # Retrieve the librarian for a library.
 def query_samples():
 
+          # List all books in a library.
           library = Library.objects.get(name="library_name")
-          
+          books = library.books.all()
+          print(books)
+
           # Query all books by a specific author.
           author = Author.objects.get(name="John Doe")
           books = author.books.all()
           print(books)
-
-          # List all books in a library.
-
-          books = library.books.all()
-          print(books)
-
 
           # Retrieve the librarian for a library.
           librarian = library.librarian.all()
