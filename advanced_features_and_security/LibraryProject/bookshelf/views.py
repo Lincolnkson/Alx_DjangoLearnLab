a8a66c56b979f65677f8251c07cb447a1305efcf
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-03-01 13:12:23 +0200

    Commint

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
 from .models import Book
+from .forms import ExampleForm
 # Create your views here.
 """
 Modify your views to check for these permissions before allowing users to perform certain actions. Use decorators such as permission_required to enforce these permissions in your views.
@@ -31,4 +32,18 @@ def create_book(request):
         publication_year = request.POST.get('publication_year')
         Book.objects.create(title=title, author=author, publication_year=publication_year)
         return HttpResponse("Book created successfully")
-    return render(request, 'bookshelf/form_example.html')
+    return render(request, 'bookshelf/form_example.html',ExampleForm)
+
+
+#Create view that contains: ["from .forms import ExampleForm"]
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        form = ExampleForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Book updated successfully")
+    else:
+        form = ExampleForm(instance=book)
+    return render(request, 'bookshelf/form_example.html', {'form': form})
