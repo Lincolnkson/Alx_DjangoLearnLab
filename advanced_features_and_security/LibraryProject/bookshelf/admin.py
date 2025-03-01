1f6690b81f12061ab625b29a9dcb68438cbeff5d
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-03-01 09:15:10 +0200

    Commint

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,30 +1,49 @@
-# from django.contrib import admin
-# from .models import Book
-
-# # Registering the Book model with the admin interface.
-# #admin.site.register(Book)  
-
-# # - LibraryProject/bookshelf/admin.py doesn't contain: ["admin.ModelAdmin"]
-# #LibraryProject/bookshelf/admin.py contains: ["admin.BookAdmin"]
-
-# class BookAdmin(admin.BookAdmin):
-#     list_display = ('title', 'author', 'published_date')
-#     search_fields = ('title', 'author')
-
-# admin.site.register(Book, BookAdmin)
-
-
-# """
-# Task Description:
-# Enhance your bookshelf app by integrating the Book model with the Django admin interface. Customize the admin display to improve the management and visibility of book data, and document the process to ensure consistent setup and configuration.
-
-# Steps:
-# 1. Register the Book Model with the Django Admin:
-# """
-
 from django.contrib import admin
-from .models import Book
+from .models import Book,CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
+
+
+class CustomUserAdmin(UserAdmin):
+    # Add the new fields to the admin display
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'date_of_birth', 'profile_photo')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Include new fields in the add user form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+    
+    # Add new fields to the list display
+    list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
+    
+    # Allow filtering by date of birth
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    
+    # Add date_of_birth to search fields
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'date_of_birth')
+
+# Register the custom user model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
 # This line registers the Book model with the admin interface.
 #admin.site.register(Book)  
 
