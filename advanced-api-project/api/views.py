e4a63c3918351c30fdff898abf7447e04343b780
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-03-15 15:15:37 +0200

    saving

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+
 #Creating generic CRUD functionality
 class BookListView(generics.ListAPIView):
     """
@@ -10,7 +11,7 @@ class BookListView(generics.ListAPIView):
     Authentication:
     - Requires user authentication via token or session
     """
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     serializer_class = BookSerializer
     def get_queryset(self):
@@ -34,7 +35,7 @@ class BookDetailView(generics.RetrieveAPIView):
     - Requires user authentication via token or session
 
     """
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     serializer_class = BookSerializer
     def get_queryset(self):
