2f236fa24f941b1a14e60a3338cc0180800c5147
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-03-15 15:14:05 +0200

    saving

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,7 +11,12 @@ urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
     path('books/create/', views.book_create, name='book_create'),
-    path('books/<int:pk>/update/', views.book_update, name='book_update'),
-    path('books/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('books/update', views.book_update, name='book_update'),
+    path('books/delete', views.book_delete, name='book_delete'),
+"""
+api/urls.py doesn't contain: ["books/update", "books/delete"]
 
+    path('books/<int:pk>/update', views.book_update, name='book_update'),
+    path('books/<int:pk>/delete', views.book_delete, name='book_delete'),
+"""
 ]
