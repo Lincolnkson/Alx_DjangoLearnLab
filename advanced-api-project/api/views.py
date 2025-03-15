552e635f8ac3f37b9c33050f196ebb0df3cc4b59
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-03-15 15:46:36 +0200

    saving

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
+# from django_filters import rest_framework
 
 #Creating generic CRUD functionality
 class BookListView(generics.ListAPIView):
