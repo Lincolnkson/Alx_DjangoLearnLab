10beff86ad6b2f3136283955ca29617e60583746
Lincolnkson <lincolnmotlalepula@gmail.com>
2025-03-21 16:07:44 +0200

    Saving

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -27,4 +29,9 @@ class UserUpdateForm(forms.ModelForm):
 #     class Meta:
 #         model = User
 #         fields = ['image']
-        
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']        
